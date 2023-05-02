a655c3f0c7573525983667c99af1b15738401686
Pedro <38092988+Toriality@users.noreply.github.com>
2023-05-02 07:17:42 -0300

    Create token schema

---
+++ token.js
@@ -0,0 +1,37 @@
+const Schema = require("mongoose").Schema;
+
+const tokenSchema = new Schema({
+  secret: {
+    type: String,
+    required: [true, "Secret is required"],
+    // Make a simple request on Spotify API to check if token is valid
+    validate: {
+      validator: async function (secret) {
+        const response = await fetch(
+          `https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${secret}`,
+            },
+          }
+        );
+
+        return response.status === 200;
+      },
+      message: "Invalid token",
+    },
+  },
+  expireAt: {
+    type: Date,
+    // Spotify API token expires after 1 hour
+    default: Date.now() + 3600 * 1000,
+    expires: "1h",
+  },
+});
+
+tokenSchema.post("save", function () {
+  console.log(`Server has saved token ${this.secret}`);
+});
+
+module.exports = tokenSchema;
